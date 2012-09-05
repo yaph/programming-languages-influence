185303c0f14e2265155532f402e5a861c20a3ead
Ramiro Gomez <web@ramiro.org>
2012-09-05 21:00:49 +0200

    on mouseover node hide all unconnected

--- sigmagraph.js
+++ sigmagraph.js
@@ -1,9 +1,10 @@
 $(function(){
-  var nodes = {};
+  var defaultColor = '#fff';
+  var nodesbyid = {}
   var sig = sigma.init(document.getElementById('sig')).drawingProperties({
-    defaultLabelColor: '#fff',
+    defaultLabelColor: defaultColor,
     defaultLabelSize: 14,
-    defaultLabelBGColor: '#fff',
+    defaultLabelBGColor: defaultColor,
     defaultLabelHoverColor: '#000',
     labelThreshold: 6,
     defaultEdgeType: 'curve'
@@ -18,21 +19,16 @@ $(function(){
 
   var addNode = function(node) {
     var nid = node['id'];
-    if (!nodes.hasOwnProperty(nid)) {
-      if (!node.hasOwnProperty('influenced')) {
-        node.influenced = [];
-      }
-      nodes[nid] = node;
-      sig.addNode(nid, {
-        x: Math.random(),
-        y: Math.random(),
-        size: node.influenced.length,
-        color: '#fff',
-        label: node.name,
-        id: nid,
-        influenced: node.influenced
-      });
-    }
+    nodesbyid[nid] = true;
+    sig.addNode(nid, {
+      x: Math.random(),
+      y: Math.random(),
+      size: node.influenced.length,
+      color: '#fff',
+      label: node.name,
+      id: nid,
+      influenced: node.influenced
+    });
   };
 
   $.getJSON('langs.json', function(data) {
@@ -42,61 +38,56 @@ $(function(){
 
     sig.iterNodes(function(n){
       $.each(n.attr.influenced, function(key, dst) {
-        if (nodes.hasOwnProperty(n.id) && nodes.hasOwnProperty(dst.id)) {
+        if (nodesbyid[n.id] && nodesbyid[dst.id]) {
           sig.addEdge(n.id + dst.id, n.id, dst.id);
         }
       });
     });
 
-    // events
-    var greyColor = '#333';
     var srcColor = '#67A9CF';
     var dstColor = '#EF8A62';
 
-    sig.bind('overnodes', function(event){
-      var nodes = event.content; // array containing hovered node
-      var neighbors = {};
+    // events
+    sig.bind('overnodes',function(event){
+      var hnode = sig.getNodes(event.content)[0];
+      if (0 == hnode.degree) return;
+      var influenced = {};
+      var influencedby = {};
       sig.iterEdges(function(e){
-        // not connected to hovered node
-        if(nodes.indexOf(e.source)<0 && nodes.indexOf(e.target)<0){
-          if(!e.attr['grey']){
-            e.attr['true_color'] = e.color;
-            e.color = greyColor;
-            e.attr['grey'] = 1;
-          }
+        if (e.source == hnode.id) {
+          e.color = srcColor;
+          influenced[e.target] = true;
+        } else if (e.target == hnode.id) {
+          e.color = dstColor;
+          influencedby[e.source] = true;
         } else {
-          if (-1 !== nodes.indexOf(e.source)) {
-            e.color = srcColor;
-          } else if (-1 !== nodes.indexOf(e.target)) {
-            e.color = dstColor;
-          }
-          e.attr['grey'] = 0;
-          neighbors[e.source] = 1;
-          neighbors[e.target] = 1;
+          e.hidden = 1;
         }
       }).iterNodes(function(n){
-        if(!neighbors[n.id]){
-          if(!n.attr['grey']){
-            n.attr['true_color'] = n.color;
-            n.color = greyColor;
-            n.attr['grey'] = 1;
+        n.forceLabel = true;
+        if (n.id != hnode.id) {
+          if (influencedby[n.id]) {
+            n.color = dstColor;
+          } else if (influenced[n.id]) {
+            n.color = srcColor;
+          } else {
+            n.hidden = 1;
+            n.forceLabel = false;
           }
-        } else {
-          n.color = n.attr['grey'] ? n.attr['true_color'] : n.color;
-          n.attr['grey'] = 0;
         }
       }).draw(2,2,2);
-    }).bind('outnodes',function(){
+    }).bind('outnodes',function(event){
+      var nodes = event.content;
       sig.iterEdges(function(e){
-        e.color = e.attr['grey'] ? e.attr['true_color'] : e.color;
-        e.attr['grey'] = 0;
+        e.color = defaultColor;
+        e.hidden = 0;
       }).iterNodes(function(n){
-        n.color = n.attr['grey'] ? n.attr['true_color'] : n.color;
-        n.attr['grey'] = 0;
+        n.color = defaultColor;
+        n.hidden = 0;
+        n.forceLabel = false;
       }).draw(2,2,2);
     });
 
     sig.draw();
-
   });
 });
